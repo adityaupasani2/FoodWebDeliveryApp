89673ca3ba8805d7186b5f30ee5604cdff03d451
adityaupasani2 <adityaupasani29@gmail.com>
2023-10-22 20:15:24 +0530

    final commit

---
+++ frontend/src/page/Cancel.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Cancel = () => {
+  return (
+    <div className="bg-red-300 w-full max-w-md m-auto h-40 flex justify-center items-center font-semibold text-lg my-10">
+      <p> PAYMENT IS CANCELLED</p>
+    </div>
+  );
+};
+
+export default Cancel;
